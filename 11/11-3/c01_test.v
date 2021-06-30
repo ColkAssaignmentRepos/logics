0b6585f540cbb0558cdeadfb3708db2bed05b1f1
Colk-tech <colk.tech@gmail.com>
2021-06-30 11:55:54 +0900

    [add] Complete the assignment "11-3"

---
+++ 11/11-3/c01_test.v
@@ -0,0 +1,43 @@
+`timescale 1ns/1ps
+module c01_test;
+
+reg a, b, c;
+wire d;
+
+circuit01 gate(a, b, c, d);
+
+initial begin
+    $dumpfile("c01_test.vcd");
+    $dumpvars(0, c01_test);
+    $monitor("%t: a=%b, b=%b, c=%b, d=%b",
+                $time, a, b, c, d);
+    
+    #0  a = 0;
+        b = 0;
+        c = 0;
+    #10 a = 0;
+        b = 0;
+        c = 1;
+    #10 a = 0;
+        b = 1;
+        c = 0;
+    #10 a = 0;
+        b = 1;
+        c = 1;
+    #10 a = 1;
+        b = 0;
+        c = 0;
+    #10 a = 1;
+        b = 0;
+        c = 1;
+    #10 a = 1;
+        b = 1;
+        c = 0;
+    #10 a = 1;
+        b = 1;
+        c = 1;
+    #10 $finish;
+
+end
+
+endmodule
